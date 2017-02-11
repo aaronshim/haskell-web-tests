7f78893e2d4fc5905f49cd381a4fe2085a723e7a
Aaron Shim <aaron.k.shim@gmail.com>
2017-02-11 00:43:31 -0800

    adding port number from environment

--- yesod/Main.hs
+++ yesod/Main.hs
@@ -1,6 +1,12 @@
 import Application () -- for YesodDispatch instance
 import Foundation
 import Yesod.Core
+import System.Environment
 
 main :: IO ()
-main = warp 3000 App
+main = do
+	-- grab env variable
+	env <- getEnvironment
+	let port = maybe 8080 read $ lookup "PORT" env
+	-- and then run the app
+	warp port App
