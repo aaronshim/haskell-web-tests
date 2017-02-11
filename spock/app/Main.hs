891f49fc8626aafa6dcd2ee1057fe893c16bb8be
Aaron Shim <aaron.k.shim@gmail.com>
2017-02-11 01:53:54 -0800

    added random

--- spock/app/Main.hs
+++ spock/app/Main.hs
@@ -7,22 +7,41 @@ import Web.Spock.Config
 import Control.Monad.Trans
 import Data.Monoid
 import Data.IORef
+import System.Random
 import qualified Data.Text as T
 
 data MySession = EmptySession
-data MyAppState = DummyAppState (IORef Int)
+data MyAppState = DummyAppState (IORef (Int, StdGen))
 
 main :: IO ()
 main =
-    do ref <- newIORef 0
+    do gen <- getStdGen
+       ref <- newIORef (0, gen)
        spockCfg <- defaultSpockCfg EmptySession PCNoDatabase (DummyAppState ref)
        runSpock 8080 (spock spockCfg app)
 
 app :: SpockM () MySession MyAppState ()
 app =
     do get root $
-           text "Hello World!"
+           do (DummyAppState ref) <- getState
+              -- we will store the stdGen in the Application State
+              stdGen <- liftIO $ atomicModifyIORef' ref $ \(i, g) -> ((i, g), g)
+              let passphrase = base64string 10 stdGen
+              -- and we are going to manually update the generator
+              newGen <- liftIO newStdGen
+              _ <- liftIO $ atomicModifyIORef' ref $ \(i, g) -> ((i, newGen), g)
+              -- display
+              text $ "Hello World!\nYour random word is " <> passphrase
        get ("hello" <//> var) $ \name ->
            do (DummyAppState ref) <- getState
-              visitorNumber <- liftIO $ atomicModifyIORef' ref $ \i -> (i+1, i+1)
+              visitorNumber <- liftIO $ atomicModifyIORef' ref $ \(i, g) -> ((i+1, g), i+1)
               text ("Hello " <> name <> ", you are visitor number " <> T.pack (show visitorNumber))
+
+-- not very efficient but whatever (later I guess we'll use a library of sorts?)
+base64string :: RandomGen g => Int -> g -> T.Text
+base64string size gen =
+    let
+        allowedChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
+        indexRange = (0, length allowedChars - 1)
+    in
+        T.pack $ map (\x -> allowedChars !! x) $ take size $ randomRs indexRange gen
