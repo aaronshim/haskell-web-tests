e269b8b4d3f9deda8309b17b0aac13204f13bed9
Aaron Shim <aaron.k.shim@gmail.com>
2017-02-14 00:05:23 -0800

    Played around with composing different APIs and different servers to serve those APIs

--- servant-project/src/Lib.hs
+++ servant-project/src/Lib.hs
@@ -20,19 +20,36 @@ data User = User
 
 $(deriveJSON defaultOptions ''User)
 
-type API = "users" :> Get '[JSON] [User]
+-- we can define APIs and then combine them with the special :<|> operator
+type UsersAPI = "users" :> Get '[JSON] [User]
+type RootAPI = Get '[PlainText] String
+type API = UsersAPI :<|> RootAPI
 
+-- Main IO action
 startApp :: IO ()
 startApp = run 8080 app
 
+-- the abstract representation of the app-- won't run until we run it with IO action
 app :: Application
 app = serve api server
 
+-- yeah I'm still not clear why this needs to be here, but it does?
 api :: Proxy API
 api = Proxy
 
+-- can combine servers with the same combinator as type-level API's BUT only in that exact order!
 server :: Server API
-server = return users
+server = usersServer :<|> rootServer
+
+-- type synonyms?
+--usersServer :: Server UsersAPI
+usersServer :: Handler [User]
+usersServer = return users
+
+-- type synonyms?
+--rootServer :: Server RootAPI
+rootServer :: Handler String
+rootServer = return "Hello, world!"
 
 users :: [User]
 users = [ User 1 "Isaac" "Newton"
